d11256d29bdc220b2a4c1cacb59c98e09d7fe0d7
U-AzureAD\ARNOLDDSOUZA <dsouza.arnold@gmail.com>
2020-01-13 10:44:32 +0530

    changed the port

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 const publicDirectoryPath = path.join(__dirname, '../public') 
 
